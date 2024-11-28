f8fa3a3a21babc53039b4fcaf4fe790987f98835
lucas-nelson-uiuc <lucas.nelson.uiuc@gmail.com>
2024-11-27 22:18:27 -0600

    test: update tests

--- tests/test_selector.py
+++ tests/test_selector.py
@@ -71,32 +71,32 @@ class TestColumnSelector:
         assert set(selected_columns).difference(expected_columns) == set()
 
     def test_temporal(self, sample_data):
-        selected_columns = sample_data.select(cs.temporal())
+        selected_columns = sample_data.select(cs.temporal()).columns
         expected_columns = ["seasons"]
         assert set(selected_columns).difference(expected_columns) == set()
 
     def test_date(self, sample_data):
-        selected_columns = sample_data.select(cs.date())
+        selected_columns = sample_data.select(cs.date()).columns
         expected_columns = ["birth_date"]
         assert set(selected_columns).difference(expected_columns) == set()
 
     def test_time(self, sample_data):
-        selected_columns = sample_data.select(cs.time())
+        selected_columns = sample_data.select(cs.time()).columns
         expected_columns = ["original_air_date"]
         assert set(selected_columns).difference(expected_columns) == set()
 
     def test_interval(self, sample_data):
-        selected_columns = sample_data.select(cs.interval())
+        selected_columns = sample_data.select(cs.interval()).columns
         expected_columns = []
         assert set(selected_columns).difference(expected_columns) == set()
 
     def test_complex(self, sample_data):
-        selected_columns = sample_data.select(cs.complex())
+        selected_columns = sample_data.select(cs.complex()).columns
         expected_columns = []
         assert set(selected_columns).difference(expected_columns) == set()
 
     def test_required(self, sample_data):
-        selected_columns = sample_data.select(cs.required())
+        selected_columns = sample_data.select(cs.required()).columns
         expected_columns = [
             "name",
             "birth_date",
@@ -106,21 +106,21 @@ class TestColumnSelector:
         assert set(selected_columns).difference(expected_columns) == set()
 
     def test_matches(self, sample_data):
-        selected_columns = sample_data.select(cs.matches("_"))
+        selected_columns = sample_data.select(cs.matches("_")).columns
         expected_columns = [
             "birth_date",
             "original_air_date",
         ]
         assert set(selected_columns).difference(expected_columns) == set()
         
-        selected_columns = sample_data.select(cs.matches("date$"))
+        selected_columns = sample_data.select(cs.matches("date$")).columns
         expected_columns = [
             "birth_date",
             "original_air_date",
         ]
         assert set(selected_columns).difference(expected_columns) == set()
         
-        selected_columns = sample_data.select(cs.matches("*"))
+        selected_columns = sample_data.select(cs.matches("*")).columns
         expected_columns = [
             "name",
             "birth_date",
@@ -131,35 +131,35 @@ class TestColumnSelector:
         assert set(selected_columns).difference(expected_columns) == set()
 
     def test_contains(self, sample_data):
-        selected_columns = sample_data.select(cs.contains("_"))
+        selected_columns = sample_data.select(cs.contains("_")).columns
         expected_columns = [
             "birth_date",
             "original_air_date",
         ]
         assert set(selected_columns).difference(expected_columns) == set()
         
-        selected_columns = sample_data.select(cs.contains("me"))
+        selected_columns = sample_data.select(cs.contains("me")).columns
         expected_columns = [
             "name",
             "instrument"
         ]
         assert set(selected_columns).difference(expected_columns) == set()
         
-        selected_columns = sample_data.select(cs.contains("krusty"))
+        selected_columns = sample_data.select(cs.contains("krusty")).columns
         expected_columns = []
         assert set(selected_columns).difference(expected_columns) == set()
 
     def test_starts_with(self, sample_data):
-        selected_columns = sample_data.select(cs.starts_with("o"))
+        selected_columns = sample_data.select(cs.starts_with("o")).columns
         expected_columns = ["original_air_date"]
         assert set(selected_columns).difference(expected_columns) == set()
         
-        selected_columns = sample_data.select(cs.starts_with("z"))
+        selected_columns = sample_data.select(cs.starts_with("z")).columns
         expected_columns = []
         assert set(selected_columns).difference(expected_columns) == set()
 
     def test_ends_with(self, sample_data):
-        selected_columns = sample_data.select(cs.ends_with("e"))
+        selected_columns = sample_data.select(cs.ends_with("e")).columns
         expected_columns = [
             "name",
             "birth_date",
@@ -167,10 +167,10 @@ class TestColumnSelector:
         ]
         assert set(selected_columns).difference(expected_columns) == set()
         
-        selected_columns = sample_data.select(cs.ends_with("date"))
+        selected_columns = sample_data.select(cs.ends_with("date")).columns
         expected_columns = ["birth_date", "original_air_date",]
         assert set(selected_columns).difference(expected_columns) == set()
         
-        selected_columns = sample_data.select(cs.ends_with("z"))
+        selected_columns = sample_data.select(cs.ends_with("z")).columns
         expected_columns = []
         assert set(selected_columns).difference(expected_columns) == set()
