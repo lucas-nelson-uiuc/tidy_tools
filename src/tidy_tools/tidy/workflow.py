2f8d653f202dfb495cdaff056f32bac684c5ed2d
lucas-nelson-uiuc <lucas.nelson.uiuc@gmail.com>
2024-11-28 10:56:16 -0600

    build: uncomment code

--- src/tidy_tools/tidy/workflow.py
+++ src/tidy_tools/tidy/workflow.py
@@ -1,45 +1,45 @@
-# from typing import Any
-# import inspect
-# import functools
+from typing import Any
+import inspect
+import functools
 
-# from attrs import define, field
+from attrs import define, field
 
-# from tidy_tools.tidy._logger import _logger
-# from tidy_tools.tidy._types import Functions, Objects
-
-
-# def identity(obj: Any) -> Any:
-#     """Return input object as is."""
-#     return obj
+from tidy_tools.tidy._logger import _logger
+from tidy_tools.tidy._types import Functions, Objects
 
 
-# def metadata_factory() -> dict:
-#     return dict(
-#         name="No name provided",
-#         description="No description provided"
-#     )
+def identity(obj: Any) -> Any:
+    """Return input object as is."""
+    return obj
 
-# @define
-# class TidyWorkFlow:
-#     input: Objects
-#     funcs: Functions
-#     preprocess: Functions = field(default=identity)
-#     postprocess: Functions = field(default=identity)
-#     metadata: dict = field(factory=metadata_factory)
 
-#     def run(self):
-#         input = map(self.preprocess, self.input)
-#         result = functools.reduce(
-#             lambda init, func: transform(init, func),
-#             self.funcs,
-#             self.input
-#         )
-#         output = self.preprocess(result)
-#         return output
+def metadata_factory() -> dict:
+    return dict(
+        name="No name provided",
+        description="No description provided"
+    )
+
+@define
+class TidyWorkFlow:
+    input: Objects
+    funcs: Functions
+    preprocess: Functions = field(default=identity)
+    postprocess: Functions = field(default=identity)
+    metadata: dict = field(factory=metadata_factory)
+
+    def run(self):
+        input = map(self.preprocess, self.input)
+        result = functools.reduce(
+            lambda init, func: transform(init, func),
+            self.funcs,
+            self.input
+        )
+        output = self.preprocess(result)
+        return output
 
     
-#     def metadata(self):
-#         return {
-#             func.__name__: inspect.getdoc(func)
-#             for func in self.funcs
-#         }
+    def metadata(self):
+        return {
+            func.__name__: inspect.getdoc(func)
+            for func in self.funcs
+        }
