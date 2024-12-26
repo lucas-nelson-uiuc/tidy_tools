4bcb94c88e876b9e2b0f44522612c3026642a26b
lucas-nelson-uiuc <lucas.nelson.uiuc@gmail.com>
2024-12-26 17:43:55 -0600

    chore: comment import init logs

--- src/tidy_tools/__init__.py
+++ src/tidy_tools/__init__.py
@@ -6,6 +6,10 @@ from loguru import logger
 __version__ = "0.3.1"
 
 
+# upon import:
+#   - remove existing logger
+#   - update logging format
+#   - log welcome message for documentation
 LOG_FORMAT: str = "{time:HH:mm:ss} | <level>{level:<8}</level> | {message}"
 logger.remove()
 logger.add(sys.stderr, format=LOG_FORMAT)
