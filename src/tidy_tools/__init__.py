46a3cd54dbd88401be3af25bf980c32fffc14a2f
lucas-nelson-uiuc <lucas.nelson.uiuc@gmail.com>
2024-12-26 17:06:14 -0600

    feat: include welcome message for documentation purposes

--- src/tidy_tools/__init__.py
+++ src/tidy_tools/__init__.py
@@ -1,13 +1,14 @@
 import sys
 
 from loguru import logger
-from tidy_tools.functions import merge
-from tidy_tools.functions import reader
 
-__all__ = ["reader", "merge"]
+
 __version__ = "0.3.1"
 
 
 LOG_FORMAT: str = "{time:HH:mm:ss} | <level>{level:<8}</level> | {message}"
 logger.remove()
 logger.add(sys.stderr, format=LOG_FORMAT)
+logger.info(
+    f"Tidy Tools imported: {__version__}. See https://lucas-nelson-uiuc.github.io/tidy_tools/ for more details."
+)
