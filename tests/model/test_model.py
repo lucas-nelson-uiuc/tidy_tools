ddf65868cae4ad3f2cad60d996e248a83af41fd6
lucas-nelson-uiuc <lucas.nelson.uiuc@gmail.com>
2024-12-30 14:26:54 -0600

    tests: include dummy read func

--- tests/model/test_model.py
+++ tests/model/test_model.py
@@ -4,12 +4,17 @@ from pyspark.sql import DataFrame
 
 class TestTidyDataModel:
     def test_load_from_source(self, simpsons_data, simpsons_model):
+        def read_func(x):
+            return x
+
         # validate returned object is a DataFrame
-        data = simpsons_model.load(simpsons_data)
+        data = simpsons_model.load(simpsons_data, read_func=read_func)
         assert isinstance(data, DataFrame)
 
         # validate multiple sources can be passed
-        data = simpsons_model.load(simpsons_data, simpsons_data, simpsons_data)
+        data = simpsons_model.load(
+            simpsons_data, simpsons_data, simpsons_data, read_func=read_func
+        )
         assert isinstance(data, DataFrame)
 
         # validate columns match model fields
