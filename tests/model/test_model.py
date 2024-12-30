69820390eabe25d211b8bbab6634af680e31a269
lucas-nelson-uiuc <lucas.nelson.uiuc@gmail.com>
2024-12-30 14:28:30 -0600

    tests: include dummy read func

--- tests/model/test_model.py
+++ tests/model/test_model.py
@@ -4,7 +4,7 @@ from pyspark.sql import DataFrame
 
 class TestTidyDataModel:
     def test_load_from_source(self, simpsons_data, simpsons_model):
-        def read_func(x):
+        def read_func(x, *args, **kwargs):
             return x
 
         # validate returned object is a DataFrame
